b658c2d269b95b42699c1b8c4b4b294311a792ba
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-01 21:44:44 +0600

    project done

--- src/api/MyAddedFoods.js
+++ src/api/MyAddedFoods.js
@@ -1,3 +1,4 @@
+// this is only for myaddedfood api
 export const myaddedFood = (email) => {
   return fetch(`${import.meta.env.VITE_API_URL}/all_foods?email=${email}`).then(
     (res) => res.json()
